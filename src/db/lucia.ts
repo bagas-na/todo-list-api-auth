91570fb2dfac733f0f9677cfe4c2e3f998d6d71f
bagas-na <bagasnurseno@gmail.com>
2024-10-01 20:03:19 +0700

    add lucia auth

---
+++ src/db/lucia.ts
@@ -0,0 +1,36 @@
+import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
+import type { Env } from "hono";
+import type { Session, User } from "lucia";
+import { Lucia } from "lucia";
+import db from "./db";
+import { SelectUsers, sessionsTable, usersTable } from "./tables";
+
+export interface Context extends Env {
+  Variables: {
+    user: User | null;
+    session: Session | null;
+  };
+}
+
+const adapter = new DrizzleSQLiteAdapter(db, sessionsTable, usersTable);
+const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      username: attributes.username
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    UserId: number;
+    DatabaseUserAttributes: SelectUsers;
+  }
+}
