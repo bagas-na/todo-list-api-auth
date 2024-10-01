eb9617c664791e034ba70414d505e671dfef5950
bagas-na <bagasnurseno@gmail.com>
2024-10-01 20:02:58 +0700

    create database table schemas and driver

--- src/db/tables.ts
+++ src/db/tables.ts
@@ -1,25 +1,51 @@
+import { SQLiteSessionTable } from "@lucia-auth/adapter-drizzle";
 import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   username: text("username").unique(),
-  passwordhash: text("password").notNull(),
+  password: text("password").notNull(), // stored as the hash
   salt: text("salt").notNull(),
-  email: text("email").unique(),
-  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`),
-  updatedAt: text("updated_at")
-    .default(sql`(CURRENT_TIMESTAMP)`)
-    .$onUpdateFn(() => new Date().toISOString().replace("T", " ").slice(0, -5)),
+  // email: text("email").unique(),
+  createdAt: integer("created_at", { mode: "timestamp_ms" })
+    .notNull()
+    .default(sql`(unixepoch() * 1000)`),
+  updatedAt: integer("created_at", { mode: "timestamp_ms" })
+    .notNull()
+    .default(sql`(unixepoch() * 1000)`)
+    .$onUpdateFn(() => new Date()),
 });
+
+export type SelectUsers = typeof usersTable.$inferSelect
+export type InsertUsers = typeof usersTable.$inferInsert
+
+export const sessionsTable = sqliteTable("sessions", {
+  id: text("id").primaryKey(),
+  userId: integer("userId")
+    .notNull()
+    .references(() => usersTable.id),
+  expiresAt: integer("expires_at").notNull(),
+}) 
+
+export type SelectSessions = typeof sessionsTable.$inferSelect
+export type InsertSessions = typeof sessionsTable.$inferInsert
 
 export const todosTable = sqliteTable("todos", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  authorId: integer("author_id").references(() => usersTable.id),
+  authorId: integer("author_id")
+    .notNull()
+    .references(() => usersTable.id),
   title: text("title").notNull(),
   description: text("description").notNull(),
-  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`),
-  updatedAt: text("updated_at")
-    .default(sql`(CURRENT_TIMESTAMP)`)
-    .$onUpdateFn(() => new Date().toISOString().replace("T", " ").slice(0, -5)),
+  createdAt: integer("created_at", { mode: "timestamp_ms" })
+    .notNull()
+    .default(sql`(unixepoch() * 1000)`),
+  updatedAt: integer("created_at", { mode: "timestamp_ms" })
+    .notNull()
+    .default(sql`(unixepoch() * 1000)`)
+    .$onUpdateFn(() => new Date()),
 });
+
+export type SelectTodos = typeof todosTable.$inferSelect
+export type InsertTodos = typeof todosTable.$inferInsert
